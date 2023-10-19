a38181a4210ea6f74e22c82360041a6ff6993582
Pablo Suárez Lapalma <84790504+P4B10SL@users.noreply.github.com>
2023-10-19 17:53:27 -0300

    Código para remover tildes y letra eñe

--- removeNonHispanicChar.py
+++ removeNonHispanicChar.py
@@ -1,5 +1,4 @@
 import unicodedata
-import re
 
 # Abre el archivo de texto con manejo de errores
 with open('Reglamentacion.txt', 'r', encoding='latin-1', errors='ignore') as file:
@@ -7,7 +6,6 @@ with open('Reglamentacion.txt', 'r', encoding='latin-1', errors='ignore') as fil
 
 # Elimina los caracteres con tilde y la letra "ñ"
 normalized_content = unicodedata.normalize('NFKD', content).encode('ASCII', 'ignore').decode('utf-8')
-normalized_content = re.sub(r'[^\w\s]', '', normalized_content)
 
 # Escribe el contenido normalizado en un nuevo archivo
 with open('Reglamentacion_sin_tildes.txt', 'w', encoding='utf-8') as new_file:
--- toJSON.py
+++ toJSON.py
@@ -1,25 +1,31 @@
+import re
 import json
 
-# Abre el archivo de texto con la codificación adecuada
-with open('Reglamentacion.txt', 'r', encoding='utf-8') as file:
-    lines = file.readlines()
+# Abre el archivo de texto
+with open('Reglamentacion.txt', 'r') as file:
+    content = file.read()
 
-# Procesa cada línea y crea un diccionario
-data_list = []
-current_data = {}
-for line in lines:
-    line = line.strip()
-    if line:
-        key, value = map(str.strip, line.split(': ', 1))
-        current_data[key] = value
-    else:
-        data_list.append(current_data)
-        current_data = {}
+# Divide el contenido del archivo en bloques separados por líneas en blanco
+blocks = re.split(r'\n\s*\n', content)
 
-# Agrega el último diccionario a la lista
-if current_data:
-    data_list.append(current_data)
+# Inicializa una lista vacía para almacenar los diccionarios resultantes
+result = []
 
-# Escribe la lista de diccionarios en un archivo JSON
-with open('Reglamentacion.json', 'w', encoding='utf-8') as json_file:
-    json.dump(data_list, json_file, ensure_ascii=False, indent=4)
+# Itera a través de los bloques
+for block in blocks:
+    # Inicializa un diccionario vacío para almacenar cada entrada
+    entry = {}
+    # Divide cada bloque en líneas separadas
+    lines = block.split('\n')
+    # Itera a través de cada línea y extrae la clave y el valor
+    for line in lines:
+        key_value = re.match(r'([^:]+):(.+)', line)
+        if key_value:
+            key = key_value.group(1).strip()
+            value = key_value.group(2).strip()
+            entry[key] = value
+    result.append(entry)
+
+# Escribe el resultado en un archivo JSON
+with open('Reglamentacion.json', 'w') as json_file:
+    json.dump(result, json_file, indent=4)
