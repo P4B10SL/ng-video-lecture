41973f17ecc68638d6423b8bdbb74299298ebdcb
Pablo Suárez Lapalma <84790504+PabloSuarezLapalma@users.noreply.github.com>
2023-11-02 17:38:18 -0300

    DatasetMix Logrado

--- ConsultasToJson.py
+++ ConsultasToJson.py
@@ -4,7 +4,11 @@ import json
 # Read the contents of the question-answer file
 with open('Consultas_formateadas.csv', 'r') as f:
     reader = csv.DictReader(f, delimiter=';')
-    qa_list = list(reader)
+    qa_list = []
+    for row in reader:
+        # Strip whitespace from each field
+        qa = {k: v.strip() for k, v in row.items()}
+        qa_list.append(qa)
 
 # Convert the list of dictionaries to JSON
 json_data = json.dumps(qa_list, indent=4)
--- combination.py
+++ combination.py
@@ -11,7 +11,7 @@ regulation_dict = {}
 pattern = r"Documento: (\d+)\nArticulo: (\d+)\nCapitulo: (.+?)\n\n(.+?)(?=\n\nDocumento: |\Z)"
 matches = re.findall(pattern, regulation_text, re.DOTALL)
 for match in matches:
-    id = f"{match[0]}-{match[1]}"
+    id = match[1]  # Use the 'Articulo' column as the ID
     title = match[2].strip()
     context = match[3].strip()
     regulation_dict[id] = {
@@ -24,25 +24,30 @@ with open('Consultas_formateadas.csv', 'r') as f:
     reader = csv.DictReader(f, delimiter=';')
     qa_list = list(reader)
 
+# Print the first few items of qa_list
+print(qa_list[:5])
+
+# Print the first few items of regulation_dict
+print(list(regulation_dict.items())[:5])
+
 # Combine the datasets
 data = []
 for qa in qa_list:
-    id = qa['idRespuestaEsperada(idArticulo)'].strip()   # Assuming 'idArticulo' is the third column
+    id = qa[' idRespuestaEsperada(idArticulo) '].strip()  # Include spaces in the key
+    print(f"Checking ID: {id}")  # Print the ID being checked
     if id in regulation_dict:
+        print(f"Found ID: {id}")  # Print the ID if it was found
         data.append({
             "id": id,
             "title": regulation_dict[id]['title'],
             "context": regulation_dict[id]['context'],
-            "question": qa['Consulta'].strip(),  # Assuming 'Question' is the second column
+            "question": qa[' Consulta'].strip(),  # Include spaces in the key
             "answers": {
                 "text": [regulation_dict[id]['context']],
                 "answer_start": [1]
             }
         })
 
-# Convert the data to JSON
-json_data = json.dumps(data, indent=4)
-
 # Write the JSON data to a file
-with open('DatasetCombinado.json', 'w') as f:
-    f.write(json_data)
+with open('output.json', 'w') as f:
+    json.dump(data, f, indent=4)
