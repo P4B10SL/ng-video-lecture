e0c84d813b5c6cb19588439c6a5d275c862a47f6
Pablo Suárez Lapalma <84790504+PabloSuarezLapalma@users.noreply.github.com>
2023-11-02 16:19:50 -0300

    Generating Train and Val dsts

---
+++ combination.py
@@ -0,0 +1,48 @@
+import re
+import json
+import csv
+
+# Read the contents of the regulation file
+with open('Reglamentacion_sin_tildes.txt', 'r') as f:
+    regulation_text = f.read()
+
+# Parse the regulation text into a dictionary
+regulation_dict = {}
+pattern = r"Documento: (\d+)\nArticulo: (\d+)\nCapitulo: (.+?)\n\n(.+?)(?=\n\nDocumento: |\Z)"
+matches = re.findall(pattern, regulation_text, re.DOTALL)
+for match in matches:
+    id = f"{match[0]}-{match[1]}"
+    title = match[2].strip()
+    context = match[3].strip()
+    regulation_dict[id] = {
+        "title": title,
+        "context": context
+    }
+
+# Read the contents of the question-answer file
+with open('Consultas.csv', 'r') as f:
+    reader = csv.reader(f, delimiter=';')
+    qa_list = [row for row in reader]
+
+# Combine the datasets
+data = []
+for qa in qa_list:
+    id = qa[2]  # Assuming 'idArticulo' is the third column
+    if id in regulation_dict:
+        data.append({
+            "id": id,
+            "title": regulation_dict[id]['title'],
+            "context": regulation_dict[id]['context'],
+            "question": qa[1],  # Assuming 'Question' is the second column
+            "answers": {
+                "text": [regulation_dict[id]['context']],
+                "answer_start": [1]
+            }
+        })
+
+# Convert the data to JSON
+json_data = json.dumps(data, indent=4)
+
+# Write the JSON data to a file
+with open('output.json', 'w') as f:
+    f.write(json_data)
